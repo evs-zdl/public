234447d1f61ab1f248eb560eaf7a591e4e85bb8e
Ashley Evans <ashley.evans@zdlgroup.com>
2024-11-14 21:35:14 +0000

    first commit

---
+++ display-on.ps1
@@ -0,0 +1,12 @@
+﻿Set-ExecutionPolicy Unrestricted CurrentUser -Force -Confirm:$false
+Clear-Host
+
+$WShell = New-Object -com "Wscript.Shell"
+
+while ($true)
+{
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Seconds 120
+}
